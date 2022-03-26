6676bbd6c88b00585842fe55306b0c6239239059
Christoph Sax <christoph.sax@gmail.com>
2022-03-26 21:38:55 +0100

    update local script

--- script/update-all.R
+++ script/update-all.R
@@ -3,7 +3,13 @@
 source("script/update-testdir.R")
 
 library(gert)
-git_branch_create("preview", checkout = TRUE)
+
+# start with fresh branch 'preview', based on 'main'
+git_branch_checkout("main")
+git_branch_delete("preview")
+git_branch_create("preview")
+
+# add file and force-push, so no history is left by frequent updates
 git_add('data/testdir.csv')
 git_commit("[local user] update from salesforce")
 git_push(force = TRUE)
