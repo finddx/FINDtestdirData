6fb425e68e9a0b1b49a93b836b560bdb695c5fb0
Christoph Sax <christoph.sax@gmail.com>
2022-03-26 23:43:49 +0100

    preview branches: one per data set

--- script/update-all.R
+++ script/update-all.R
@@ -6,7 +6,7 @@ library(gert)
 
 # start with fresh branch 'preview', based on 'main'
 
-git_branch_create("preview")
+git_branch_create("preview-testdir")
 
 # add file and force-push, so no history is left by frequent updates
 git_add('data/testdir.csv')
@@ -14,4 +14,4 @@ git_commit("[local user] update from salesforce")
 git_push(force = TRUE)
 
 git_branch_checkout("main")
-git_branch_delete("preview")
+git_branch_delete("preview-testdir")
