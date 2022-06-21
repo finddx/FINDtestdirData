a72fb4b7eeb751fc96587614699d70fbab3da6a5
benubah <ben@rpowerlabs.org>
2022-06-21 19:28:41 +0100

    include script to process mpx updates

---
+++ script/update-mpx_testdir.R
@@ -0,0 +1,50 @@
+
+library(readr)
+library(tidyr)
+library(dplyr)
+library(fuzzyjoin)
+
+
+sf_data <- readr::read_csv("https://raw.githubusercontent.com/finddx/FINDtestdirData/main/data/raw/mpx_testdir_data.csv")
+
+
+meta_cols <-
+  readr::read_csv("data/mpx_testdir_meta_cols.csv", show_col_types = FALSE) |>
+  filter(salesforce_name %in% colnames(sf_data))
+
+data_raw <-
+  sf_data |>
+  select({ meta_cols$salesforce_name }) |>
+  rename_with(~ meta_cols$id, meta_cols$salesforce_name)
+
+
+country_info <- shinyfind::get_country_info()
+
+country_map <-
+  countrycode::codelist %>%
+  as_tibble() %>%
+  select(
+    regex = country.name.en.regex, alpha3 = iso3c
+  ) |>
+  left_join(select(country_info, alpha3, country = name, continent), by = "alpha3")
+
+
+extract_link <- function(x) {
+  x <- gsub("[^[|^]+]" ,"", x)
+  x <- gsub("[[()]" ,"", x)
+  x
+}
+
+data <-
+  data_raw |>
+  mutate(across(everything(), ~na_if(., "-"))) |>
+  rename(name = country) |>
+  fuzzyjoin::regex_left_join(country_map, by = c("name" = "regex"), ignore_case = TRUE) |>
+  select(-name, -alpha3) |>
+  rename(region = continent) |>
+  mutate(permalink = extract_link(permalink))|>
+  mutate(submission_id = row_number()) |>
+  relocate(submission_id, .before = submission_title)
+
+
+write_csv(data, "data/mpx_testdir.csv")
