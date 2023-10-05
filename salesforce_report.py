86325498ef513711c7f622f88c20ad99ac1e2905
JuanVallarta <juan.vallarta@finddx.org>
2023-10-05 15:51:36 +0200

    Add COVID new arch report

--- salesforce_report.py
+++ salesforce_report.py
@@ -11,7 +11,8 @@ reports = {
 	"00O6900000CGKWKEA5": "eqa_covid_testdir",
 	"00O6900000CGaQTEA1": "amr_testdir",
 	"00O6900000BrsNLEAZ": "outbreaks_testdir",
-	"00O6900000BweVREAZ": "outbreaks_new"
+	"00O6900000BweVREAZ": "outbreaks_new",
+	"00O6900000BwgQTEAZ": "testdir_new"
 }
 
 if len(sys.argv) < 2:
