a0ccdc0d092d6fccc7ff17a194b8429fcb06f8d1
JuanVallarta <juan.vallarta@finddx.org>
2023-10-13 16:17:04 +0200

    Update salesforce_report.py

--- salesforce_report.py
+++ salesforce_report.py
@@ -12,7 +12,8 @@ reports = {
 	"00O6900000CGaQTEA1": "amr_testdir",
 	"00O6900000BrsNLEAZ": "outbreaks_testdir",
 	"00O6900000BweVREAZ": "outbreaks_new",
-	"00O6900000BwgQTEAZ": "testdir_new"
+	"00O6900000BwgQTEAZ": "testdir_new",
+	"00O6900000BwcuoEAB": "tb_testdir"
 }
 
 if len(sys.argv) < 2:
